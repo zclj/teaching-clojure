8c67c317115d0c1ab39d43abb11416f5239acb14
amorokh <amorokh@foo.bar>
2020-09-25 12:01:40 +0200

    Updated spec examples during session

--- spec/src/examples/spec.clj
+++ spec/src/examples/spec.clj
@@ -16,6 +16,7 @@
 ;; even more information
 (s/explain even? 10)
 (s/explain even? 9)
+(s/explain-data even? 9)
 
 ;; a set can be used as a predicate
 (s/valid? #{:foo :bar :baz} :bar)
@@ -45,6 +46,7 @@
 (s/valid? ::even-positive 9)
 
 (s/explain ::even-positive -2)
+(s/explain-data ::even-positive -2)
 
 (s/def ::name-or-id (s/or :name string? :id int?))
 (s/valid? ::name-or-id "foobar")
@@ -57,3 +59,28 @@
 ;; generating values still works
 (gen/sample (s/gen ::name-or-id))
 (map #(s/conform ::name-or-id %) (gen/sample (s/gen ::name-or-id)))
+
+
+;; spec maps
+(s/def ::name string?)
+(s/def ::nick string?)
+(s/def ::id int?)
+(s/def ::person (s/keys :req [::name ::id]
+                        :opt [::nick]))
+
+(s/valid? ::person {::name "foo" ::id 12 :bar "tjosan"})
+(s/valid? ::person {::name "foo" ::id 12 ::even-positive 14})
+(s/conform ::person {::name "foo" ::id 12 ::even-positive 14})
+
+(gen/generate (s/gen ::person))
+
+;; spec collections
+(s/def ::person-2 (s/cat :id ::id :name ::name :nick ::nick :person ::person))
+
+(gen/generate (s/gen ::person-2))
+(s/conform ::person-2 (gen/generate (s/gen ::person-2)))
+
+;; modules/{module}/desired/model/{id}
+(s/def ::topic (s/cat :modules #{"modules"} :module string? :desired #{"desired"} :model #{"model"} :object-id int?))
+
+(gen/generate (s/gen ::topic))
