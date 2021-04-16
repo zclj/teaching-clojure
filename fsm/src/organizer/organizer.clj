6de42f34555e27aa6233bff5745ed42775b7f87d
amorokh <amorokh@foo.bar>
2021-04-16 15:17:08 +0200

    Updated after session

--- fsm/src/organizer/organizer.clj
+++ fsm/src/organizer/organizer.clj
@@ -1,78 +1,94 @@
 (ns organizer
-  (:require [clojure.pprint :refer [pprint]]))
+  (:require [clojure.pprint :refer [pprint]]
+            [fsm]))
+
+(defn edit-new
+  [state item]
+  (assoc state :todos/new item))
+
+(defn commit-new
+  [state]
+  (-> state
+      (assoc-in [:todos (:todos/new state)] {})
+      (dissoc :todos/new)))
+
+(defn cancel-new
+  [state]
+  (dissoc state :todos/new))
+
+(defn rem-item
+  [state item]
+  (update state :todos dissoc item))
 
 (defonce app-state (atom {:state :init}))
 
-(def fsm {:init           {:start      :main}
-          :main           {:todos      :todos}
-          :todos          {:has-items  :todos/existing
-                           :no-items   :todos/empty}
-          :todos/empty    {:edit-new   :todos/empty
-                           :commit-new :todos/existing
-                           :main       :main}
-          :todos/existing {:edit-new   :todos/existing
-                           :commit-new :todos/existing
-                           :ack-item   :todos/existing
-                           :rem-item   :todos
-                           :main       :main}})
+(def fsm {:init            {:start      {:next :todos/check}}
+          :todos/check     {:has-items  {:next :todos/some}
+                            :no-items   {:next :todos/empty}}
+          :todos/empty     {:edit-new   {:next :todos/empty-new
+                                         :fn   edit-new}}
+          :todos/empty-new {:commit-new {:next :todos/some
+                                         :fn   commit-new}
+                            :cancel-new {:next :todos/empty
+                                         :fn   cancel-new}}
+          :todos/some      {:edit-new   {:next :todos/some-new
+                                         :fn   edit-new}
+                            :ack-item   {:next :todos/some}
+                            :rem-item   {:next :todos/check
+                                         :fn   rem-item}}
+          :todos/some-new  {:commit-new {:next :todos/some
+                                         :fn   commit-new}
+                            :cancel-new {:next :todos/some
+                                         :fn   cancel-new}}})
 
-(defn dispatch-trans
-  [from trans to & _]
-  [from trans to])
+(def do-transition (partial fsm/do-transition fsm app-state))
 
-(defmulti transition-fn #'dispatch-trans)
+(defn handle-todos-check!
+  [state]
+  (if (seq (:todos state))
+    (do-transition :todos/check :has-items)
+    (do-transition :todos/check :no-items)))
 
-(defmethod transition-fn :default
-  [_ _ to state]
-  (assoc state :state to))
+(defn start
+  []
+  (reset! app-state {:state :init})
+  (fsm/subscribe app-state :todos/check handle-todos-check!))
 
-;; (defmethod transition-fn [:init :start :main]
-;;   [_ _ _ state]
-;;   (assoc state :state :main))
+(defn stop
+  []
+  (fsm/unsubscribe app-state :todos/check))
 
-;; (defmethod transition-fn [:main :todos :todos]
-;;   [_ _ _ state]
-;;   (assoc state :state :todos))
-
-;; (defmethod transition-fn [:todos :has-items :todos/existing]
-;;   [_ _ _ state]
-;;   (assoc state :state :todos/existing))
-
-;; (defmethod transition-fn [:todos :no-items :todos/empty]
-;;   [_ _ _ state]
-;;   (assoc state :state :todos/empty))
-
-(defmethod transition-fn [:todos/empty :edit-new :todos/empty]
-  [_ _ _ state item]
-  (-> state
-      (assoc :state :todos/empty)
-      (assoc :todos/new item)))
-
-
-
-(defn do-transition
-  [from trans & args]
-  (let [to (get-in fsm [from trans])]
-    (swap! app-state (fn [s]
-                       (if (= (:state s) from)
-                         (apply transition-fn from trans to s args)
-                         s)))))
-
+(defn restart
+  []
+  (stop)
+  (start))
 
 (comment
   ;;
 
+  (add-watch app-state :foo (fn [_ _ _ n] (pprint n)))
+  (remove-watch app-state :foo)
+
+  (restart)
+
   @app-state
   (deref app-state)
-  (reset! app-state {:state :main})
+
+  (reset! app-state {:state :init})
+
+  (do-transition :init :foo)
 
   (do-transition :init :start)
-
-  (do-transition :main :foo)
-  (do-transition :main :todos)
-  (do-transition :todos :no-items)
-
+  ;; (do-transition :todos/check :no-items)
   (do-transition :todos/empty :edit-new "bar")
+  (do-transition :todos/empty-new :commit-new)
+  (do-transition :todos/some :edit-new "baz")
+  (do-transition :todos/some-new :cancel-new)
+  (do-transition :todos/some-new :commit-new)
+
+  (do-transition :todos/some :rem-item "baz")
+  (do-transition :todos/some :rem-item "bar")
+
 
   ;;  
   )
